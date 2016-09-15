96f6392a7b668732f5b8a970554e7d9baadb413c
Yair Ogen <yaogen@cisco.com>
2016-09-15 11:39:11 +0300

    add default properties for pure spring boot env

---
+++ master-slave/src/main/java/com/cisco/oss/foundation/cluster/masterslave/MasterSlaveConfiguration.java
@@ -0,0 +1,12 @@
+package com.cisco.oss.foundation.cluster.masterslave;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Yair Ogen (yaogen) on 15/09/2016.
+ */
+@Configuration
+@PropertySource("classpath:master-slave.properties")
+public class MasterSlaveConfiguration {
+}
