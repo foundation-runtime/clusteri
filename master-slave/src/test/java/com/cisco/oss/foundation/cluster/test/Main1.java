1c3d6bb318c5efb5f92a48d100045786074b66f5
Yair Ogen <yaogen@cisco.com>
2017-07-24 11:03:01 +0300

    add test code

---
+++ master-slave/src/test/java/com/cisco/oss/foundation/cluster/test/Main1.java
@@ -0,0 +1,42 @@
+package com.cisco.oss.foundation.cluster.test;
+
+import com.cisco.oss.foundation.cluster.registry.MasterSlaveListener;
+import com.cisco.oss.foundation.cluster.registry.MasterSlaveRegistry;
+import com.cisco.oss.foundation.configuration.CcpConstants;
+import com.cisco.oss.foundation.environment.utils.EnvUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Yair Ogen (yaogen) on 13/04/2016.
+ */
+public class Main1 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main1.class);
+
+    public static void main(String[] args) {
+        System.setProperty(CcpConstants.RPM_SOFTWARE_NAME,"comp1");
+        EnvUtils.updateEnv(CcpConstants.FQDN,"localhost");
+        EnvUtils.updateEnv("STACK_NAME","test");
+        EnvUtils.updateEnv("STACK_VERSION","001");
+        MasterSlaveRegistry.INSTANCE.addMasterSlaveListener("test1", new MasterSlaveListener() {
+            @Override
+            public void goMaster() {
+                LOGGER.info("[TEST1] - I AM MASTER");
+            }
+
+            @Override
+            public void goSlave() {
+                LOGGER.info("[TEST1] - I AM slave");
+            }
+        });
+
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
